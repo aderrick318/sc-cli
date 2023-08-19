ca0b240ed89b9d38db672ed4450f25ac9452c306
ThisGuy169 <aderrick318@gmail.com>
2023-08-18 20:37:30 -0500

    Project Init

---
+++ cmd/root.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2023 NAME HERE aderrick318@gmail.com
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "sc-cli",
+	Short: "cli tool for opening various areas of Support Center directly",
+	Long: `Opens the corresponding edit pages for Issues, Location, Person records, and Sales Orders.`,
+	Run: func(cmd *cobra.Command, args []string) { 
+
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+}
+
+
